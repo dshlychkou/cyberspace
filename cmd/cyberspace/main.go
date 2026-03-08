56a2590cd30dd5495eba18856af8b16fc9d4ff5a
dshlychkou <4901549+dshlychkou@users.noreply.github.com>
2026-03-08 20:34:34 +0100

    refactor: clean up graceful shutdown

--- cmd/cyberspace/main.go
+++ cmd/cyberspace/main.go
@@ -5,7 +5,7 @@ import (
 	"log"
 	"os/signal"
 	"syscall"
-
+	
 	tea "charm.land/bubbletea/v2"
 	"github.com/barnowlsnest/go-configlib/v2/pkg/configs"
 	"github.com/dshlychkou/cyberspace/internal/game"
@@ -15,28 +15,28 @@ import (
 func main() {
 	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
 	defer stop()
-
+	
 	cfg := &game.Config{}
 	if _, err := configs.Resolve(cfg, "cyberspace"); err != nil {
-		exitErr("Config error: %v", err)
+		log.Fatalf("Config error: %v", err)
 	}
-
+	
 	model := tui.NewModel(ctx, *cfg)
-
 	p := tea.NewProgram(model)
-
+	
+	var err error
+	doneChan := make(chan struct{})
 	go func() {
+		defer close(doneChan)
 		<-ctx.Done()
-		p.Send(tea.QuitMsg{})
+		stop()
 	}()
-
-	if _, err := p.Run(); err != nil {
-		exitErr("Error running game: %v", err)
+	go func() {
+		<-doneChan
+		p.Quit()
+	}()
+	
+	if _, err = p.Run(); err != nil {
+		log.Fatalf("Error running game: %v", err)
 	}
-
-	model.Shutdown()
-}
-
-func exitErr(format string, args ...any) {
-	log.Fatalf(format, args...)
 }
